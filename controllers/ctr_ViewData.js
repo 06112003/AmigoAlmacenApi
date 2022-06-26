0ea74911a9aed4c7d7ae60ffeaa5de45084873df
Fredy <fredy12cvpu@gmail.com>
2022-06-26 16:55:37 -0500

    sadass

--- controllers/ctr_ViewData.js
+++ controllers/ctr_ViewData.js
@@ -4,13 +4,17 @@ const db = Database.db(process.env.DB)
 const Controladores = {
 
 
-    ViewPrincipal: (req, res)=>{
+    ViewPrincipal: async(req, res)=>{
         //Filtros generales
         var Categoria = req.body.Categoria || ''        
         var Busqueda = req.body.Busqueda || ''
         //Obtener la pagina
         var Pagina = parseInt(req.body.Pagina) || 1
         var minRangoPage = (Pagina - 1) * 6
+        var dataPaginador = {
+            PageMax:  0,
+            PageAct: Pagina
+        }
         //Filtros de orden
         var Stock = parseInt(req.body.Stock) || 0
         var Orden = {}        
@@ -18,37 +22,18 @@ const Controladores = {
             Orden.stock = Stock
         }else{
             Orden.idProducto = -1            
-        }   
-        var GenFilt = {
-            $and: [
-                {categoria: {$regex: Categoria}}, 
-                {producto:  {$regex: Busqueda, "$options" : "i"}}
-            ]
-        }                                         
-        db.collection('Lista_Productos').find(GenFilt).limit(6).skip(minRangoPage).sort(Orden).toArray((err, data)=>{
-            if(data || !err){
-                //Variable con los productos
-                const ArrayData = data 
-                //---
-                db.collection('Lista_Productos').find(GenFilt).toArray.count((err, data)=>{
-                    if(data && !err){
-                        console.log(data)
-                        //Datos para el paginador
-                        const ctnResult = Math.ceil(parseInt(data) / 6)
-                        const dataPaginador = {
-                            PageMax:  ctnResult,
-                            PageAct: Pagina
-                        }
-                        //---
-                        res.status(200).json({Estado: true, Mensaje: 'Se encontraron los productos', dato: ArrayData, pageData: dataPaginador})
-                    }else{
-                        res.status(404).json({Estado: false, Mensaje: 'No pudieron encontrar el total de registros dentro de la base de datos', dato: null, pageData: null})        
-                    }
-                })                                
-            }else{
-                res.status(404).json({Estado: false, Mensaje: 'No tiene ningun producto con esos filtros', dato: null, pageData: null})
-            }
-        })
+        }
+        //Generando la consulta
+        try{                                                     
+            var consultDB =  await db.collection('Lista_Productos').find({$and: [{categoria: {$regex: Categoria}}, {producto:  {$regex: Busqueda, "$options" : "i"}}]}).sort(Orden)            
+            var ctnResult = await consultDB.count()
+            var dataEnv =   await consultDB.skip(minRangoPage).limit(6).toArray()                                  
+            dataPaginador.PageMax = Math.ceil(ctnResult / 6) 
+            res.status(200).json({Estado: true, Mensaje: 'Se encontraron los datos del producto con exito', dato: dataEnv, dataPaginador})
+        }catch(err){
+            console.log(err)
+            res.status(404).json({Estado: false, Mensaje: 'Ocurrio un error al generar su consulta', dato: [], dataPaginador})
+        }
     },
 
 
